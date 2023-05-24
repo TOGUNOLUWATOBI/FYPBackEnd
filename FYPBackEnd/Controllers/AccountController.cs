83fd9cfd28598e91d83cc705f03900ad840a3bfe
Oluwatobi Togun <oluwatobitogun@gmail.com>
2023-05-24 14:47:27 +0100

    updated account interface and created controller for account service

---
+++ FYPBackEnd/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using FYPBackEnd.Services.Interfaces;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FYPBackEnd.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountController : Controller
+    {
+        private readonly IAccountService accountService;
+        private readonly IGoogleDrive googleDrive;
+        public static IWebHostEnvironment _environment;
+        private readonly ILogger<AuthenticationController> log;
+
+        public AccountController(IAccountService accountService, IGoogleDrive googleDrive, ILogger<AuthenticationController> log, IWebHostEnvironment environment)
+        {
+            this.accountService = accountService;
+            this.googleDrive = googleDrive;
+            this.log = log;
+            _environment = environment;
+        }
+
+
+
+    }
+}
--- FYPBackEnd/Services/Interfaces/IAccountService.cs
+++ FYPBackEnd/Services/Interfaces/IAccountService.cs
@@ -1,6 +1,19 @@
-﻿namespace FYPBackEnd.Services.Interfaces
+﻿using FYPBackEnd.Data.Entities;
+using FYPBackEnd.Data.Models.RequestModel;
+using FYPBackEnd.Data.ReturnedResponse;
+using System.Threading.Tasks;
+
+namespace FYPBackEnd.Services.Interfaces
 {
     public interface IAccountService
     {
+        Task<ApiResponse> AddPanicModePin(AddPanicModePinModel model, string userId);
+        Task<ApiResponse> GenerateAccountNumber(ApplicationUser user, string bvn);
+        Task<ApiResponse> InitiateTransfer(TransferRequestModel model, string userId);
+        Task<ApiResponse> BuyAirtimeData(BuyAirtimeRequestModel model, string userId);
+        Task<ApiResponse> CheckTransactionPanicPin(CheckTransactionPinModel model, string userId);
+        Task<ApiResponse> ChangeTransactionPin(ChangeTransactionPinModel model, string userId);
+        Task<ApiResponse> AddTransactionPin(AddTransactionPinModel model, string userId);
+        Task<ApiResponse> ChangePanicModePin(ChangePanicModePinModel model, string userId);
     }
 }
