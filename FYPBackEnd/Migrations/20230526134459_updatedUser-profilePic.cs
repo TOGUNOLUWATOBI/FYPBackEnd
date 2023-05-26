96108c42eb310eda8a9ebd261c35829326393547
Oluwatobi Togun <oluwatobitogun@gmail.com>
2023-05-26 14:45:52 +0100

    Added migration for user profile pic

---
+++ FYPBackEnd/Migrations/20230526134459_updatedUser-profilePic.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FYPBackEnd.Migrations
+{
+    public partial class updatedUserprofilePic : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProficePictureId",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProficePictureId",
+                table: "AspNetUsers");
+        }
+    }
+}
