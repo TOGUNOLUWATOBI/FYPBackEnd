ce0f56bf94147c0bf173e2c67bd99fa48ccf8b35
Oluwatobi Togun <oluwatobitogun@gmail.com>
2023-06-23 06:50:23 +0100

    added migrations

---
+++ FYPBackEnd/Migrations/20230623054939_update_user_ispanicpinset.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FYPBackEnd.Migrations
+{
+    public partial class update_user_ispanicpinset : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPanicPINSet",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPanicPINSet",
+                table: "AspNetUsers");
+        }
+    }
+}
