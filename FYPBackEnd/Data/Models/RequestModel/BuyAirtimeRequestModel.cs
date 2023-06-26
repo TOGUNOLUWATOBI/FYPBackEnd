e3e5c9d5f36da23d787bec20d528f475a30e339c
Oluwatobi Togun <oluwatobitogun@gmail.com>
2023-06-26 16:23:18 +0100

    update

--- FYPBackEnd/Data/Models/RequestModel/BuyAirtimeRequestModel.cs
+++ FYPBackEnd/Data/Models/RequestModel/BuyAirtimeRequestModel.cs
@@ -2,14 +2,13 @@
 {
     public class BuyAirtimeRequestModel
     {
-        public string Country { get; set; }
+        
         public string Customer { get; set; } // this is the phone number you want to buy the airtime or data for
         public string Type { get; set; }
         public int Amount { get; set; }
         public decimal TrxAmount { get; set; }
         public decimal TransferAmount { get; set; }
-        public string Recurrence { get; set; }
-        public string Reference { get; set; }
+
         public string longitude { get; set; }
         public string latitude { get; set; }
     }
